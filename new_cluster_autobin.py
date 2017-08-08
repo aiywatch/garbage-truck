8296f77be993b31bba35a6ff598dbdf278106773
aiywatch <aiycp35@gmail.com>
2017-08-08 13:39:39 +0700

    new cluster update

--- new_cluster_autobin.py
+++ new_cluster_autobin.py
@@ -1,8 +1,9 @@
-import requests
 import pandas as pd
 import numpy as np
 import datetime
+import requests
 import connection
+import utils
 from sklearn.cluster import DBSCAN
 from shapely.geometry import MultiPoint
 from haversine import haversine
@@ -64,7 +65,7 @@ def find_representative_bins(auto_bin):
     cluster_labels = db.labels_
     num_clusters = len(set(cluster_labels))
     clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])
-#    clusters_bin_ids = pd.Series([auto_bin['id'][cluster_labels == n] for n in range(num_clusters)])
+    clusters_bin_ids = pd.Series([auto_bin['id'][cluster_labels == n] for n in range(num_clusters)])
     
     
     print('Number of clusters: {}'.format(num_clusters))
@@ -78,99 +79,137 @@ def find_representative_bins(auto_bin):
     
     lats, lons = zip(*centroid_points)
     rep_points = pd.DataFrame({'lon':lons, 'lat':lats})
-    rep_points['clustered_from'] = clusters
+    rep_points['clustered_from_bin_id'] = clusters_bin_ids
+    rep_points['clustered_from_lat_lon'] = clusters
     rep_points['type'] = 'representative'
+    
     
     return rep_points
 
 found_rep_points = find_representative_bins(auto_bin)
 
-rep_bin_mg['lonlat'] = tuple(zip(rep_bin_mg.lon, rep_bin_mg.lat))
-ll = [(found_rep_points.iloc[0]['lon'], found_rep_points.iloc[0]['lat'])] * rep_bin_mg.shape[0]
-ll2 = pd.DataFrame({'rep': rep_bin_mg['lonlat'], 'found':ll})
-distances = ll2.apply(lambda point: haversine(point['found'], point['rep']), axis=1)
-recognized_bin = distances[distances < MAXIMUM_RECOGNIZED_DISTANCE]
 
+
+#recognized_bin = distances[distances < MAXIMUM_RECOGNIZED_DISTANCE]
+
+closest_bins = []
+closest_distances = []
 for i, found_point in found_rep_points.iterrows():
-    print([(found_point['lon'], found_point['lat'])] * rep_bin_mg.shape[0])
+    rep_bin_mg['lonlat'] = tuple(zip(rep_bin_mg.lon, rep_bin_mg.lat))
+    ll = [(found_point['lon'], found_point['lat'])] * rep_bin_mg.shape[0]
+    ll2 = pd.DataFrame({'rep': rep_bin_mg['lonlat'], 'found':ll})
+    distances = ll2.apply(lambda point: haversine(point['found'], point['rep']), axis=1)
+    closest_bin, closest_distance = rep_bin_mg.loc[distances.argmin()], distances.min()
+    closest_bins += [closest_bin]
+    closest_distances += [closest_distance]
     
-    print()
+closest_bins = pd.DataFrame(closest_bins)
+closest_distances = np.array(closest_distances)
+
+
+adding_bins = found_rep_points[closest_distances > MAXIMUM_RECOGNIZED_DISTANCE]
+updating_bins = found_rep_points[(closest_distances <= MAXIMUM_RECOGNIZED_DISTANCE) &
+                                 (closest_distances > 0)]
+updating_ids = closest_bins[(closest_distances <= MAXIMUM_RECOGNIZED_DISTANCE) &
+                                 (closest_distances > 0)]
+
+updating_bins['_id'] = updating_ids['_id']
+updating_bins['bin_id'] = updating_ids['bin_id']
+
+
+sipp_token = utils.get_sipp_token()
+
+def update_bin(sipp_token, bid, bin_data):
+    """ mark a bin as unused in Sipp's Django trash API
+    """
+    headers = {
+        'Authorization': 'Bearer '+sipp_token,
+        'Content-Type':'application/json'            
+    }       
+    r = requests.patch('https://api.traffy.xyz/v0/trash/'+bid+'/', json=bin_data, headers=headers)
+    
+    return r.json()['id']
+
+
+if updating_bins.shape[0] > 0:
+    representative_auto_detected_bin = connection.connect_mongo_representative_auto_detected_bin()
+    for i, rep_point in updating_bins.iterrows():
+        ## Update Postgres
+        bin_data = {
+                'coords': [rep_point['lon'],rep_point['lat']],
+                'name': "auto_detected_bin",
+                'name_th':u'ถังถูกตรวจพบอัตโนมัติ',
+                'type':'auto'
+            }
+        
+        update_bin(sipp_token, rep_point['bin_id'], bin_data)
+        
+        ## update MongoDB
+        dict_mg = {'bin_id': rep_point['bin_id'],
+                   'lat': rep_point['lat'],
+                   'lon': rep_point['lon'],
+                   'clustered_from_bin_id': rep_point['clustered_from_bin_id'].tolist(),
+                   'clustered_from_lat_lon': rep_point['clustered_from_lat_lon'].tolist(),
+                   'type': rep_point['type'],
+                   'updated_date': datetime.datetime.now(),}
+    #    rep_point = rep_points_mg.loc[i].to_json(orient="columns")
+        representative_auto_detected_bin.update_one({'_id': rep_point['_id']}, dict_mg)
+        print("Updated {} to mongoDB".format(rep_point['bin_id']))
+        
+    
+else:
+    print('No bin is updated!')
+
+
+
+
     
 
 
+def add_new_bin(sipp_token, bin_data):
+    """ add a bin to Sipp's Django trash API
+    """
+    headers = {
+        'Authorization': 'Bearer '+sipp_token,
+        'Content-Type':'application/json'            
+    }       
+    r = requests.post('https://api.traffy.xyz/v0/trash/', json=bin_data, headers=headers)
+    print(r.content)
+    return r.json()['id']
 
-rep_bin_mg.shape[0]
-
-
-
-
-### save to postgres
-#auto_bin_raw_df = pd.DataFrame(auto_bin_raw)
-#
-#
-#rep_points_pg = pd.DataFrame({"coords": list(rep_points[["lon", "lat"]].values),
-#                              "name": "auto_detected_bin", 
-#                              'name_th': u'ถังถูกตรวจพบอัตโนมัติ',
-#                              "type": "auto"})
-#
-#sipp_token = utils.get_sipp_token()
-#def add_new_bin(sipp_token, bin_data):
-#    """ add a bin to Sipp's Django trash API
-#    """
-#    headers = {
-#        'Authorization': 'Bearer '+ sipp_token,
-#        'Content-Type':'application/json'            
-#    }       
-#    r = requests.post('https://api.traffy.xyz/v0/trash/', data=bin_data, headers=headers)
-#    print(r.content)
-#    return r.json()['id']
-#
-#bin_id = []
-#for i in rep_points_pg.index:
-#    bin_data = rep_points_pg.loc[i].to_json(orient="columns") #, force_ascii=False)
-#    print(bin_data)
-#    
-#    bin_id += [add_new_bin(sipp_token, bin_data)]
-#
-#temp_bin_id = bin_id
-#rep_points['bin_id'] = bin_id
-#rep_points_mg = rep_points[['bin_id', 'lat', 'lon', 'clustered_from', 'type']]
-#
-#
-##rep_points_mg_json = rep_points_mg.to_dict(orient="records")
-#
-#representative_auto_detected_bin = connection.connect_mongo_representative_auto_detected_bin()
-#representative_auto_detected_bin.insert_many(rep_points_mg_json)
-#
-#for i, rep_point in rep_points_mg.iterrows():
-#    dict_mg = {'bin_id': rep_point['bin_id'],
-#               'lat': rep_point['lat'],
-#               'lon': rep_point['lon'],
-#               'clustered_from_bin_id': clusters_bin_ids[i].tolist(),
-#               'clustered_from_lat_lon': rep_point['clustered_from'].tolist(),
-#               'type': rep_point['type'],
-#               'posted_date': datetime.datetime.now(),}
-##    rep_point = rep_points_mg.loc[i].to_json(orient="columns")
-#    representative_auto_detected_bin.insert_one(dict_mg)
-#
-#    print(dict_mg)
-#
-#import datetime
-#
-#repbins = []
-#for repbin in representative_auto_detected_bin.find():
-#    repbins += [repbin]
-#
-#repbins_df = pd.DataFrame(repbins)
-#
-#for i, rep_point in repbins_df.iterrows():
-#    representative_auto_detected_bin.update_one(
-#            {'bin_id': rep_point['bin_id']},
-#            {'posted_date': datetime.datetime.now()})
-#
-##representative_auto_detected_bin.delete_many({})
-#    
-#    
-############################ End
+if adding_bins.shape[0] > 0:
+    representative_auto_detected_bin = connection.connect_mongo_representative_auto_detected_bin()
+    for i, rep_point in adding_bins.iterrows():
+        ## Adding to Postgres
+        bin_data = {
+                        'coords': [rep_point['lon'],rep_point['lat']],
+                        'name': "auto_detected_bin",
+                        'name_th':u'ถังถูกตรวจพบอัตโนมัติ',
+                        'type':'auto'
+                    }
+        
+        bin_id = add_new_bin(sipp_token, bin_data)
+        
+        ## Adding to MongoDB
+        dict_mg = {'bin_id': bin_id,
+                   'lat': rep_point['lat'],
+                   'lon': rep_point['lon'],
+                   'clustered_from_bin_id': rep_point['clustered_from_bin_id'].tolist(),
+                   'clustered_from_lat_lon': rep_point['clustered_from_lat_lon'].tolist(),
+                   'type': rep_point['type'],
+                   'posted_date': datetime.datetime.now(),}
+    #    rep_point = rep_points_mg.loc[i].to_json(orient="columns")
+        representative_auto_detected_bin.insert_one(dict_mg)
     
-    
+        print("Updated {} to mongoDB".format(rep_point['bin_id']))
+else:
+    print('No new bin is found!')
+
+
+
+
+
+
+
+
+
